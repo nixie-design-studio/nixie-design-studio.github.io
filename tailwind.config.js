6ed77bef22540c6be828ac41afc4df64f7363cdf
nixie design studio <app@nixiedesignstudio.com>
2020-09-13 18:24:32 -0700

    About section is complete and responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
     backgroundColor: theme => ({
       'primary': '#333',
       'secondary': '#000',
-      'tertiary': '#f04',
+      'tertiary': '#f03',
       'danger': '#f04'
     }),
     fontFamily: theme => ({
@@ -22,9 +22,19 @@ module.exports = {
         'sans-serif'
       ]
     }),
+    boxShadow: theme => ({
+      xs: '0 0 0 1 rgba(0, 0, 0, 0.2)',
+      sm: '0 1px 2px 0 rgba(0, 0, 0, 0.2)',
+      default: '0 0.25rem 0.5rem rgba(0, 0, 0, 0.9)',
+      md: '0 0.5rem 1rem rgba(0, 0, 0, 0.75)',
+      lg: '0 1rem 2rem rgba(0, 0, 0, 0.5)',
+      xl: '0 2rem 4rem rgba(0, 0, 0, 0.25)',
+      inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
+      none: 'none'
+    }),
     textColor: theme => ({
       'primary': '#fff',
-      'secondary': '#f04',
+      'secondary': '#f03',
       'tertiary': '#333',
       'danger': '#fff'
     }),
